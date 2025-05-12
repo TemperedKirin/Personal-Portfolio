17e6b5bc4ac996a0a4103ca572fe6ae1617fce1b
Sergio <sergio.rsal@gmail.com>
2025-05-12 15:59:45 -0600

    Added more certificate cards

--- js/banner-script.js
+++ js/banner-script.js
@@ -14,3 +14,26 @@ const randomIndex = Math.floor(Math.random() * bannerImages.length);
 const randomImage = bannerImages[randomIndex];
 
 document.getElementById('banner').style.background = `url(${randomImage}) center center no-repeat`;
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.getElementById("modalClose");
+
+document.querySelectorAll(".certificate-img").forEach((img) => {
+  img.addEventListener("click", () => {
+    modalImg.src = img.dataset.full;
+    modal.classList.add("show");
+  });
+
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("show");
+  }
+});
